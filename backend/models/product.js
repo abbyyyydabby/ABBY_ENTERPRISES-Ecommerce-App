580b85f8c5673f8a62c4c514457cc5b8cd79f003
Abhinav Sagar <www.alphastriker123@gmail.com>
2025-09-28 10:42:42 +0530

    Create product.js

---
+++ backend/models/product.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    brand: String,
+    stock: {
+        type: Number,
+        default: 0,
+    },
+    rating: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5,
+    },
+    numReviews: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    weaviateId: {
+        type: String,
+        unique: true,
+        sparse: true,
+    },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
